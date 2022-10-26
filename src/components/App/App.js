1ab39b3d45e2c3e047e7169284ea6efa498af870
Yotam7298 <yoyo729800@gmail.com>
2022-10-26 21:45:35 +0300

    created navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
