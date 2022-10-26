2b064e3529e1d9156e6c50b721da34a6948ef056
Yotam7298 <yoyo729800@gmail.com>
2022-10-26 23:59:30 +0300

    fixed wrong navbar design

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import signOutImage from '../../images/logout.svg'
+// import signOutHome from '../../images/logout.svg'
+// import signOutSaved from '../../images/logout-white.svg'
 
 export default function Navbar() {
     return (
         <div className='navbar'>
             <button className='navbar__logo'>NewsExplorer</button>
-            <Link to='/' className='navbar__linkbox'>
+            <Link to='/' className='navbar__linkbox navbar__linkbox_selected'>
                 <p className='navbar__link'>Home</p>
             </Link>
-            <Link to='saved-news' className='navbar__linkbox navbar__linkbox_selected'>
+            <Link to='saved-news' className='navbar__linkbox'>
                 <div className='navbar__link'>Saved Articles</div>
             </Link>
             <button className='navbar__signin'>Sign In</button>
             {/* <button className='navbar__signout'>
                 Elise
-                <img className='navbar__signout-icon' alt='Icon for logging out' src={signOutImage} />
+                <img className='navbar__signout-icon' alt='Icon for logging out' src={signOutSaved} />
             </button> */}
         </div>
     )
