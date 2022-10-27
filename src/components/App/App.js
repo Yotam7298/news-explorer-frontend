22129310689066cd2c605c472b43b780e2f850e2
Yotam7298 <yoyo729800@gmail.com>
2022-10-27 20:29:39 +0300

    created about section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,14 @@ import Header from '../Header/Header';
 import Navbar from '../Navbar/Navbar';
 import SearchForm from '../SearchForm/SearchForm';
 import SavedNewsHeader from '../SavedNewsHeader/SavedNewsHeader';
+import About from '../About/About';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Header navbar={Navbar} searchForm={SearchForm} /> */}
+    <div className="app">
+      <Header navbar={Navbar} searchForm={SearchForm} />
       <SavedNewsHeader navbar={Navbar} />
+      <About />
     </div>
   );
 }
