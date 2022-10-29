68909795e3ac2c123d95f90769655b47e1100f75
Yotam7298 <yoyo729800@gmail.com>
2022-10-30 01:21:32 +0200

    NewsCard function improved

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -5,9 +5,9 @@ import remove from '../../images/remove.svg';
 import removeHover from '../../images/remove_hover.svg';
 import cardImage from '../../images/example-image1.png';
 
-export default function NewsCard() {
+export default function NewsCard(props) {
     const [isHover, setIsHover] = React.useState(false);
-    const [isSaved, setIsSaved] = React.useState(false);
+    const [isMessageShow, setIsMessageShow] = React.useState(false);
 
     function functionHovered() {
         setIsHover(true)
@@ -17,28 +17,42 @@ export default function NewsCard() {
         setIsHover(false)
     }
 
+    function flipMessageShow() {
+        setIsMessageShow(!isMessageShow);
+    }
+
     return (
         <div className='news-card'>
             <div className='news-card__keyword'>Keyword</div>
-            <div className='news-card__function'>
-                {isSaved ?
-                    <img
-                    src={isHover ? removeHover : remove}
-                    alt='bookmark button'
-                    onMouseEnter={functionHovered}
-                    onMouseLeave={functionEndHover}
-                    className='news-card__function-icon'
-                />
+                {props.isSaved ?
+                    <div className='news-card__function'>
+                        <img
+                        src={isHover ? removeHover : remove}
+                        alt='bookmark button'
+                        onMouseEnter={functionHovered}
+                        onMouseLeave={functionEndHover}
+                        onClick={flipMessageShow}
+                        className='news-card__function-icon'
+                        />
+                        <div className={`news-card__function-message ${isMessageShow ? 'news-card__function-message_show' : ''}`}>
+                            Remove from saved
+                        </div>
+                    </div>
                     :
-                    <img
-                    src={isHover ? bookmarkHover : bookmark}
-                    alt='bookmark button'
-                    onMouseEnter={functionHovered}
-                    onMouseLeave={functionEndHover}
-                    className='news-card__function-icon'
-                />}
-                <div className='news-card__function-message'>Sign in to save article</div>
-            </div>
+                    <div className='news-card__function'>
+                        <img
+                        src={isHover ? bookmarkHover : bookmark}
+                        alt='bookmark button'
+                        onMouseEnter={functionHovered}
+                        onMouseLeave={functionEndHover}
+                        onClick={flipMessageShow}
+                        className='news-card__function-icon'
+                        />
+                        <div className={`news-card__function-message ${isMessageShow ? 'news-card__function-message_show' : ''}`}>
+                            Sign in to save article
+                        </div>
+                    </div>
+                }
             <img src={cardImage} alt='illustrator image for news article' className='news-card__image' />
             <div className='news-card__text'>
                 <p className='news-card__date'>November 4, 2020</p>
